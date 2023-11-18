6a93364926b676e9c8f82ae7d64a19318d17decc
andreybel1989 <bav7269213@gmail.com>
2023-11-18 19:26:30 +0300

    second task

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,61 +1,59 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+﻿// // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// // 0, 7, 8, -2, -2 -> 2
+// // 1, -7, 567, 89, 223-> 3
 
-using System.Globalization;
+// using System.Globalization;
 
-int GetDesiredNumber()
-{
-    Console.WriteLine("Enter the desired number:");
-    int M = Convert.ToInt32(Console.ReadLine());
-    return M;
-}
+// int GetDesiredNumber()
+// {
+//     Console.WriteLine("Enter the desired number:");
+//     int M = Convert.ToInt32(Console.ReadLine());
+//     return M;
+// }
 
-int number = GetDesiredNumber();
+// int number = GetDesiredNumber();
 
-int[] GetArrayNumber(int size)
-{
-    int[] arrayNumber = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        Console.WriteLine("enter number");
-        arrayNumber[i] = Convert.ToInt32(Console.ReadLine());
-    }
-    return arrayNumber;
-}
+// int[] GetArrayNumber(int size)
+// {
+//     int[] arrayNumber = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.WriteLine("enter number");
+//         arrayNumber[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return arrayNumber;
+// }
 
-int SelectArray(int[] arr)
-{    
-    int result = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i]>0)
-        {
-            result++;
-        }
+// int SelectArray(int[] arr)
+// {    
+//     int result = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i]>0)
+//         {
+//             result++;
+//         }
         
-    }
-    return result;
-}
+//     }
+//     return result;
+// }
 
 
-void PrintArray(int[] arr)
-{
-    Console.WriteLine($"Array: [{String.Join(", ", arr)}]");
-}
-void PrintResult(int res)
-{
-    Console.WriteLine($"number > 0 =  {res}");
-}
-int [] array = GetArrayNumber(number);
+// void PrintArray(int[] arr)
+// {
+//     Console.WriteLine($"Array: [{String.Join(", ", arr)}]");
+// }
+// void PrintResult(int res)
+// {
+//     Console.WriteLine($"number > 0 =  {res}");
+// }
+// int [] array = GetArrayNumber(number);
 
-void MainFunction(int[] arr)
-{
-    PrintArray(arr);
-    PrintResult(SelectArray(arr));
-}
+// void MainFunction(int[] arr)
+// {
+//     PrintArray(arr);
+//     PrintResult(SelectArray(arr));
+// }
 
-MainFunction(array);
-
-
+// MainFunction(array);
 
---
+++ Example002/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int size = 2;
+
+int[] GetFirstLineCoefficient(int size)
+{
+    int[] firstLine = new int[size];
+    Console.WriteLine("Input coefficient k1");
+
+    firstLine[0] = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Input coefficient b1");
+    firstLine[1] = Convert.ToInt32(Console.ReadLine());
+
+    return firstLine;
+
+}
+
+int[] GetSecondLineCoefficient(int size)
+{
+    int[] secondLine = new int[size];
+    Console.WriteLine("Input coefficient k2");
+
+    secondLine[0] = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Input coefficient b2");
+    secondLine[1] = Convert.ToInt32(Console.ReadLine());
+
+    return secondLine;
+
+}
+
+bool CheckLine(int[] coefficientFirstLine, int[] coefficientSecondLine)
+{
+    if (coefficientFirstLine[0] == coefficientSecondLine[0])
+    {
+
+        if (coefficientFirstLine[1] == coefficientSecondLine[1])
+        {
+            Console.WriteLine("Lines are coincide");
+            return false;
+        }
+
+        Console.WriteLine("Lines are parallel");
+        return false;
+    }
+    return true;
+
+
+}
+
+void GetInterdictionCoordinate(int size,  int[] firstLine, int[] secondLine)
+{
+    double[] coordinate = new double[size];
+
+    coordinate[0] = (double)(secondLine[1] - firstLine[1]) / (double)(firstLine[0] - secondLine[0]);
+    coordinate[1] = firstLine[0] * coordinate[0] + firstLine[1];
+
+    Console.WriteLine($"coordinate interdiction line: X = {coordinate[0]} Y = {coordinate[1]}");
+}
+
+int[] firstLine = GetFirstLineCoefficient(size);
+int[] secondLine = GetSecondLineCoefficient(size);
+
+if(CheckLine(firstLine, secondLine))
+{
+    GetInterdictionCoordinate(size, firstLine, secondLine);
+}
+
